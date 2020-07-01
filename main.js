bf6d26a462f2c7cae4272b1daf94b7e10ab5fea4
tamburkey <petem14@gmail.com>
2020-06-30 23:24:12 -0400

    comments/format

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// define some functions which we can use as callbacks
 function add(a, b) {
     return a + b
 }
@@ -10,73 +11,52 @@ function divide(a, b) {
     return a / b
 }
 
-// we want a function which takes in 2 numbers and a callback function, and then calls the callback function on the 2 numbers and returns that result
-
+// define a function which takes in 2 numbers and a callback function, and then calls the callback function on the 2 numbers and returns that result, replaced by arrayCalculator
 function calculator(num1, num2, callback) {
     return callback(num1, num2)
 }
 
-
-function addCalculatorEvent(targetElement, callback) {
-    targetElement.addEventListener("click", function () {
-        // get the inputs from the input elements
-        let inputArray1 = input1.value.split(",")
-        let inputArray2 = input2.value.split(",")
-        // put the inputs into the calculator function with the add callback
-        let result = arrayCalculator(inputArray1, inputArray2, callback)
-        // take that result and put it into the output div
-        let resultDiv = document.createElement('div')
-        if(callback === add){
-            resultDiv.append("addition: ")
-        }else if(callback === multiply){
-            resultDiv.append("multiplication: ")
-        }else if(callback === divide){
-            resultDiv.append("division: ")
-        }
-        resultDiv.append(result)
-        outputDiv.append(resultDiv)
-    })
-}
-
-addCalculatorEvent(additionButton, add)
-addCalculatorEvent(multiplyButton, multiply)
-addCalculatorEvent(divisionButton, divide)
-
 function arrayCalculator(array1, array2, callback) {
     let finalArray = []
+    // TODO: fix bug when array2.length > array1.length
     for (let i = 0; i < array1.length; i = i + 1) {
-        let currentNumber1 = Number(array1[i].split(" ").join(""))
-        let currentNumber2 = Number(array2[i].split(" ").join(""))
+        let currentNumber1 = Number(array1[i].split(" ").join("")) // .split(" ").join("") removes all spaces
+        let currentNumber2 = Number(array2[i].split(" ").join("")) // .split(" ").join("") removes all spaces
         let resultNumber = callback(currentNumber1, currentNumber2)
         finalArray.push(resultNumber)
     }
     return finalArray
 }
 
+function handleClick(callback) {
+    // get the inputs from the input elements
+    let inputArray1 = input1.value.split(",")
+    let inputArray2 = input2.value.split(",")
 
-// let testArray = [1,2,3,4,5,6, -1,-5,6]
+    // put the inputs into the calculator function with the add callback
+    let result = arrayCalculator(inputArray1, inputArray2, callback)
 
-// function addFive(a){
-//     return a + 5
-// }
+    // create and append result div
+    let resultDiv = document.createElement('div')
+    if (callback === add) {
+        resultDiv.append("addition: ")
+    } else if (callback === multiply) {
+        resultDiv.append("multiplication: ")
+    } else if (callback === divide) {
+        resultDiv.append("division: ")
+    }
+    resultDiv.append(result)
+    outputDiv.append(resultDiv)
+}
 
-// function isGreaterThan3(a){
-//     if(a > 3){
-//         return true
-//     } else {
-//         return false
-//     }
-// }
+// add the click event listener to the target element
+function addCalculatorEvent(targetElement, callback) {
+    // pass the callback function to the handleClick within an anonymous function definition
+    // ref: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Event_listener_with_anonymous_function
+    targetElement.addEventListener("click", function () { handleClick(callback) })
+}
 
-// // console.log(testArray.map(addFive))
-
-// console.log(testArray.filter(isGreaterThan3))
-
-// let finalArray = []
-// for(let i = 0; i < testArray.length; i=i+1){
-//     let currentItem = testArray[i]
-//     if(isGreaterThan3(currentItem) === true){
-//         finalArray.push(currentItem)
-//     }
-// }
-// console.log(finalArray)
+// call the addCalculatorEvent function on the button elements from our view.js file
+addCalculatorEvent(additionButton, add)
+addCalculatorEvent(multiplyButton, multiply)
+addCalculatorEvent(divisionButton, divide)
